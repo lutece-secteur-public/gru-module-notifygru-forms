991bffc09fb14d48dcce3cac4455606001835019
Guillaume Lépine <guillaume.lepine@sogeti.com>
2021-04-26 16:00:14 +0200

    MNGRUF-9: use new GenericFormsProvider

--- src/java/fr/paris/lutece/plugins/notifygru/modules/forms/services/provider/FormsProvider.java
+++ src/java/fr/paris/lutece/plugins/notifygru/modules/forms/services/provider/FormsProvider.java
@@ -33,63 +33,35 @@
  */
 package fr.paris.lutece.plugins.notifygru.modules.forms.services.provider;
 
-import fr.paris.lutece.plugins.forms.business.Form;
-import fr.paris.lutece.plugins.forms.business.FormQuestionResponse;
-import fr.paris.lutece.plugins.forms.business.FormResponse;
-import fr.paris.lutece.plugins.forms.business.FormResponseHome;
-import fr.paris.lutece.plugins.forms.business.Question;
-import fr.paris.lutece.plugins.forms.business.QuestionHome;
-import fr.paris.lutece.plugins.forms.util.FormsConstants;
-import fr.paris.lutece.plugins.forms.web.admin.MultiviewFormResponseDetailsJspBean;
-import fr.paris.lutece.plugins.genericattributes.business.ResponseHome;
-import fr.paris.lutece.plugins.genericattributes.service.entrytype.EntryTypeServiceManager;
-import fr.paris.lutece.plugins.genericattributes.service.entrytype.IEntryTypeService;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.collections.CollectionUtils;
-
+import fr.paris.lutece.plugins.forms.business.FormResponse;
+import fr.paris.lutece.plugins.forms.business.QuestionHome;
+import fr.paris.lutece.plugins.forms.service.provider.GenericFormsProvider;
 import fr.paris.lutece.plugins.modulenotifygrumappingmanager.business.NotifygruMappingManager;
 import fr.paris.lutece.plugins.modulenotifygrumappingmanager.business.NotifygruMappingManagerHome;
+import fr.paris.lutece.plugins.notifygru.modules.forms.services.INotifyGruFormsService;
 import fr.paris.lutece.plugins.notifygru.modules.forms.services.NotifyGruFormsService;
+import fr.paris.lutece.plugins.workflow.service.provider.ProviderManagerUtil;
 import fr.paris.lutece.plugins.workflowcore.business.resource.ResourceHistory;
 import fr.paris.lutece.portal.service.spring.SpringContextService;
 import fr.paris.lutece.portal.service.util.AppException;
-import fr.paris.lutece.portal.service.util.AppLogService;
-import fr.paris.lutece.portal.service.util.AppPathService;
 import fr.paris.lutece.portal.service.util.AppPropertiesService;
 import fr.paris.lutece.util.ReferenceList;
-import fr.paris.lutece.util.url.UrlItem;
-import fr.paris.lutece.plugins.notifygru.modules.forms.services.INotifyGruFormsService;
-import fr.paris.lutece.plugins.workflow.service.provider.ProviderManagerUtil;
-import fr.paris.lutece.plugins.workflowcore.service.provider.IProvider;
-import fr.paris.lutece.plugins.workflowcore.service.provider.InfoMarker;
-import fr.paris.lutece.portal.service.i18n.I18nService;
 
 /**
  * This class represents a provider for a {@link Forms} object
  *
  */
-public class FormsProvider implements IProvider
+public class FormsProvider extends GenericFormsProvider
 {
     // PROPERTY KEY
     private static final String PROPERTY_SMS_SENDER_NAME = "workflow-notifygruforms.gruprovider.sms.sendername";
-    // MESSAGE
-    private static final String MESSAGE_DESCRIPTION = "module.notifygru.forms.marker.provider.url.detail.reponse.description";
 
-    // MARKS
-    private static final String MARK_POSITION = "position_";
-    private static final String MARK_URL_ADMIN_RESPONSE = "url_admin_forms_response_detail";
     // PARAMETERS
     public static final String PARAMETER_VIEW_FORM_RESPONSE_DETAILS = "view_form_response_details";
     public static final String PARAMETER_ID_FORM_RESPONSES = "id_form_response";
+    
     // SERVICES
     private static INotifyGruFormsService _notifyGruFormsService = SpringContextService.getBean( NotifyGruFormsService.BEAN_SERVICE );
 
@@ -100,9 +72,7 @@ public class FormsProvider implements IProvider
     private final String _strCustomerPhoneNumber;
     private final String _strDemandReference;
     private final String _strDemandTypeId;
-    private final FormResponse _formResponse;
     private final NotifygruMappingManager _mapping;
-    private final HttpServletRequest _request;
 
     /**
      * Constructor
@@ -112,14 +82,13 @@ public class FormsProvider implements IProvider
      * @param strProviderId
      *            the provider id. Corresponds to the {@code Forms} id. Used to retrieve the mapping.
      * @param resourceHistory
-     *            the resource history. Corresponds to the {@link Record} object containing the data to provide
+     *            the resource history. Corresponds to the {@link FormResponse} object containing the data to provide
      * @param request
      *            the request
      */
     public FormsProvider( String strProviderManagerId, String strProviderId, ResourceHistory resourceHistory, HttpServletRequest request )
     {
-        // Get the form response from the resourceHistory
-        _formResponse = FormResponseHome.findByPrimaryKey( resourceHistory.getIdResource( ) );
+        super( resourceHistory, request );
 
         // Load the mapping manager
         _mapping = NotifygruMappingManagerHome.findByPrimaryKey( ProviderManagerUtil.buildCompleteProviderId( strProviderManagerId, strProviderId ) );
@@ -136,17 +105,8 @@ public class FormsProvider implements IProvider
         _strCustomerPhoneNumber = _notifyGruFormsService.getSMSPhoneNumber( _mapping, _formResponse );
         _strDemandReference = _notifyGruFormsService.getDemandReference( _mapping, _formResponse );
         _strDemandTypeId = String.valueOf( _notifyGruFormsService.getIdDemandType( _mapping ) );
-        _request = request;
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public String provideDemandId( )
-    {
-        return String.valueOf( _formResponse.getId( ) );
-    }
 
     /**
      * {@inheritDoc}
@@ -218,80 +178,6 @@ public class FormsProvider implements IProvider
     public String provideCustomerMobilePhone( )
     {
         return _strCustomerPhoneNumber;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Collection<InfoMarker> provideMarkerValues( )
-    {
-        Collection<InfoMarker> result = new ArrayList<>( );
-
-        List<FormQuestionResponse> listFormQuestionResponse = _notifyGruFormsService.getListFormQuestionResponse( _formResponse );
-
-        Map<Integer, InfoMarker> markers = new HashMap<>( );
-        for ( FormQuestionResponse formQuestionResponse : listFormQuestionResponse )
-        {
-            InfoMarker notifyMarker = markers.computeIfAbsent( formQuestionResponse.getQuestion( ).getId( ), ( k ) -> new InfoMarker( MARK_POSITION
-                    + formQuestionResponse.getQuestion( ).getId( ) ) );
-            IEntryTypeService entryTypeService = EntryTypeServiceManager.getEntryTypeService( formQuestionResponse.getQuestion( ).getEntry( ) );
-            String value;
-            if ( CollectionUtils.isEmpty( formQuestionResponse.getEntryResponse( ) ) )
-            {
-                value = "";
-            }
-            else
-            {
-				value = formQuestionResponse.getEntryResponse( ).stream( )
-						.map( response -> entryTypeService.getResponseValueForRecap(
-								formQuestionResponse.getQuestion( ).getEntry( ), _request, response, null ) )
-						.collect( Collectors.joining( ", " ) );
-            }
-            if ( notifyMarker.getValue( ) == null )
-            {
-                notifyMarker.setValue( value );
-            }
-            else
-            {
-                notifyMarker.setValue( notifyMarker.getValue( ) + "<br>" + value );
-            }
-            AppLogService.debug( "Adding infomarker " + notifyMarker.getMarker( ) + "=" + notifyMarker.getValue( ) );
-        }
-        result.addAll( markers.values( ) );
-
-        InfoMarker notifyMarkerUrl = new InfoMarker( MARK_URL_ADMIN_RESPONSE );
-        UrlItem url = new UrlItem( AppPathService.getBaseUrl( _request ) + MultiviewFormResponseDetailsJspBean.CONTROLLER_JSP_NAME_WITH_PATH );
-        url.addParameter( FormsConstants.PARAMETER_TARGET_VIEW, PARAMETER_VIEW_FORM_RESPONSE_DETAILS );
-        url.addParameter( PARAMETER_ID_FORM_RESPONSES, _formResponse.getId( ) );
-        notifyMarkerUrl.setValue( url.getUrl( ) );
-        result.add( notifyMarkerUrl );
-        return result;
-    }
-
-    /**
-     * Get the collection of InfoMarker, for the given form
-     * 
-     * @param form
-     *            The form
-     * @return the collection of the notifyMarkers
-     */
-    public static Collection<InfoMarker> getProviderMarkerDescriptions( Form form )
-    {
-        Collection<InfoMarker> collectionNotifyMarkers = new ArrayList<>( );
-
-        List<Question> listFormQuestions = QuestionHome.getListQuestionByIdForm( form.getId( ) );
-
-        for ( Question formQuestion : listFormQuestions )
-        {
-            InfoMarker notifyMarker = new InfoMarker( MARK_POSITION + formQuestion.getId( ) );
-            notifyMarker.setDescription( formQuestion.getTitle( ) );
-            collectionNotifyMarkers.add( notifyMarker );
-        }
-        InfoMarker notifyMarkerURl = new InfoMarker( MARK_URL_ADMIN_RESPONSE );
-        notifyMarkerURl.setDescription( I18nService.getLocalizedString( MESSAGE_DESCRIPTION, I18nService.getDefaultLocale( ) ) );
-        collectionNotifyMarkers.add( notifyMarkerURl );
-        return collectionNotifyMarkers;
     }
 
     /**
